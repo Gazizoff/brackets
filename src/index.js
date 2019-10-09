8fd952665fb2fefc476ceec0680ddb29e73a8813
Hazizau Kanstantsin <47750585+Gazizoff@users.noreply.github.com>
2019-10-09 14:19:22 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        let symbol = str[i];
+        let arrInConfig;
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (bracketsConfig[j].indexOf(symbol) >= 0) {
+                arrInConfig = j;
+                break;
+            };
+          };
+        if (arr.length === 0) {
+            arr.push(symbol);
+        }
+        else if (arr[arr.length-1] === bracketsConfig[arrInConfig][0] && symbol === bracketsConfig[arrInConfig][1]) {
+            arr.pop();
+        }        
+        else {
+            arr.push(symbol);
+        }                   
+    }
+    return arr.length === 0;
 }
